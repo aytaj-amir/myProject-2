1dcf922532de12f5f5418d62d1ab3cf02da0f87c
aytaj-amir <aytac.amirova1@gmail.com>
2021-04-14 03:25:26 +0400

    files

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+ var mnav = document.querySelector('.mobilenav');
+ var menu = document.querySelector('.menu');
+
+ mnav.addEventListener('click',function(){
+    menu.classList.toggle('navactive');
+ });
+
